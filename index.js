9f1932aceb7bfdb461ea51814b16833e8446c07c
Andor1298 <palinkasandor@turr.hu>
2024-05-28 12:29:55 +0200

    oldal reszponzivitása

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function calc(){
-    
+    let erk = Date(document.getElementById("erk").value);
+    let tav = Date(document.getElementById("tav").value);
+    let napoksz;
+    let szoba = document.querySelector('input[name="szoba"]:checked')
 }
